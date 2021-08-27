cbfde32c601234fe1c94f4233d7aedb44baaf331
= <=>
2021-08-26 23:36:30 -0300

    finalizado a api

---
+++ Controllers/UseController.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+using Shop.Services;
+
+namespace Shop.Controllers
+{
+    [Route("users")]
+    public class UserController : Controller
+    {
+
+        [HttpGet]
+        [Route("")]
+        [Authorize(Roles = "manager")]
+        public async Task<ActionResult<List<User>>> Get([FromServices] DataContext context)
+        {
+            var users = await context.Users.AsNoTracking().ToListAsync();
+
+            return users;
+
+        }
+
+        [HttpPost]
+        [Route("")]
+        [AllowAnonymous]
+        public async Task<ActionResult<User>> Post(
+                    [FromServices] DataContext context,
+                    [FromBody] User model)
+        {
+            if (ModelState.IsValid is not true)
+                return BadRequest(ModelState);
+
+            try
+            {
+                context.Users.Add(model);
+                await context.SaveChangesAsync();
+
+                model.Password = "";
+                return model;
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível criar o usuário" });
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<dynamic>> Authenticate(
+                    [FromServices] DataContext context,
+                    [FromBody] User model)
+        {
+
+            var user = await context.Users
+                    .AsNoTracking()
+                    .Where(x => x.Username == model.Username && x.Password == model.Password)
+                    .FirstOrDefaultAsync();
+
+            if (user == null)
+            {
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+            }
+
+            var token = TokenService.GenerateToken(user);
+
+            user.Password = "";
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,7 @@
+namespace Shop
+{
+    public static class Settings
+    {
+        public static string Secret = "b9b32dba-f512-4e29-a223-48a055fa6a89";
+    }
+}
