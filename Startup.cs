d1eb1e6137ea333c42ffba5e590580d73376c440
= <=>
2021-08-27 23:04:21 -0300

    teste developer

--- Startup.cs
+++ Startup.cs
@@ -65,12 +65,12 @@ namespace Shop
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //if (env.IsDevelopment())
-            //{
-            app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shop Api v1"));
-            //}
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Shop Api v1"));
+            }
 
             app.UseHttpsRedirection();
 
